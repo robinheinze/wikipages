c7cdb7cc769428d2dbe1e70a81a9ca4ad2918714
Robin Miller & Alex Wood <student@epicodus.com>
2014-03-31 10:01:03 -0700

    first commit. creates wikipages app by following the lesson

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,45 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    @contact = Contact.new
+    render('contacts/index.html.erb')
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render('contacts/show.html.erb')
+  end
+
+  def create
+    @contact = Contact.create(:name => params[:name],
+                              :email => params[:email],
+                              :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = '5a9e23b184c6cf0bf428dff354bc82506b8e9317cfc4704351c7bd9c1d5dea9d5fa24f05b4eb659fd63213282a0a5de31e32d63eb40e61a4715ea0013de5c291'
